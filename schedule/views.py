b4bfd5596a26d61457ee99d66bf7b4a3b4388f4a
KooYS <tlfkthsl42@naver.com>
2018-05-15 03:04:48 +0900

    hosting2

--- schedule/views.py
+++ schedule/views.py
@@ -125,11 +125,10 @@ def message(request):
 		})
 	if return_str == "알고리즘":
 		photourl = foo(request.session['kakao_id'])
-		logger.error(photourl)
 		logger.error(request.session['kakao_id'])
 		logger.error(mainurl+photourl)
 		return JsonResponse({ 
-			'message': {"text" : "", "photo" : {"url" : ""+mainurl+photourl, "width" : 630,"height" : 720},"message_button": {"label": "크게 보기","url": mainurl+"static/img/"+photourl}},
+			'message': {"text" : "", "photo" : {"url" : mainurl+"static/img/"+photourl, "width" : 630,"height" : 720},"message_button": {"label": "크게 보기","url": mainurl+"static/img/"+photourl}},
 			'keyboard': {'type': 'buttons','buttons': ['시간표 설정','알고리즘']}
 		})
 
