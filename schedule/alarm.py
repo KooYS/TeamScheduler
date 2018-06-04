050b53ac5f05cb4ed7c87100a2507930cd00abee
KooYS <tlfkthsl42@naver.com>
2018-06-04 13:19:40 +0900

    kakaomessage

--- schedule/alarm.py
+++ schedule/alarm.py
@@ -12,7 +12,6 @@ def find(auth,teamcode):
 	data = {}
 	headers = {'Authorization': 'Bearer %s' % auth}
 	arguments = {'template_id':{'9502'},'template_args':json.dumps({'title':teamcode+"팀",'content':"팀플 시간이 얼마 남지 않았습니다.","path":"static/img/"+photourl})}
-	logger.error(requests.post("https://kapi.kakao.com/v1/user/me", headers=headers).text)
 	print(arguments)
 	resp = requests.post("https://kapi.kakao.com/v2/api/talk/memo/send", headers=headers, data=arguments, verify=False)
 	print("response status:\n%d" % resp.status_code)
